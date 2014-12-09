c2e8741e69d2d3ca399e47b928e1284959f2d7fa
Ellie Day <heyellieday@gmail.com>
2014-12-09 13:08:15 -0600

    rendering

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -3,12 +3,13 @@ class CallsController < ApplicationController
 
    	skip_before_action :verify_authenticity_token
 
+
 	def create
 		response = Twilio::TwiML::Response.new do |r|
         	r.Say "Hi, this is Ellie's voicemail. She has decided to go Numberless. Please visit heyellieday dot com to get in touch with her."
      	end
-
-     	render_twiml response
+     	response.headers["Content-Type"] = "text/xml"
+     	render text: response.text
 
 	end
 
