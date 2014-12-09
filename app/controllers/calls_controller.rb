5d996c3a8eb801361b2032e0efd7db5894eab936
Ellie Day <heyellieday@gmail.com>
2014-12-09 12:56:53 -0600

    Added calls controller

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -4,12 +4,11 @@ class CallsController < ApplicationController
    	skip_before_action :verify_authenticity_token
 
 	def create
-
 		response = Twilio::TwiML::Response.new do |r|
-        r.Play "Hi, this is Ellie's voicemail. She has decided to go Numberless. Please visit heyellieday dot com to get in touch with her."
-     end
+        	r.Play "Hi, this is Ellie's voicemail. She has decided to go Numberless. Please visit heyellieday dot com to get in touch with her."
+     	end
 
-     render_twiml response
+     	render_twiml response
 
 	end
 
